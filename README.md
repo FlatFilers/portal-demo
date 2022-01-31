074dbe41253850649fbf30ef08e8c82696eaa18c
Alnoor Pirani <alnoor@Alnoors-MacBook-Pro.local>
2022-01-31 17:29:16 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# portal-demo
+(update with more details, add comments to .env.example)
+
+# Get started with Flatfile Portal 3.0 SDK in Node.js
+
+## Step 1: Create embed
+
+1. Go to the [Flatfile app](https://app.flatfile.io/)
+2. Create your Flatfile account (if you haven't already)
+3. Create a Data Template
+4. Create and configure your embed
+
+## Step 2: Configure .env
+
+1. Open `.env`
+2. Enter `EMBED_ID` and `PRIVATE_KEY` values from the embed in your Flatfile account
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const EMBED_ID = process.env.EMBED_ID;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+app.set("view engine", "html");
+app.set("views", __dirname);
+app.use(express.static(__dirname + "/public"));
+app.engine("html", require("ejs").renderFile);
+
+app.get("/", async (_req, res) => {
+  return res.render("index.html", {
+    token: jwt.sign(
+      {
+        embed: EMBED_ID,
+        user: {
+          id: "23796",
+          email: "alnoor@flatfile.io",
+          name: "Alnoor Pirani",
+        },
+        org: {
+          id: "27173",
+          name: "Team Pirani",
+        },
+      },
+      PRIVATE_KEY
+    ),
+  });
+});
+
+app.listen(process.env.PORT, "127.0.0.1");
